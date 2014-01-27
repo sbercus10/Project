2b53945ebd572f5dee5dcc7cff82658e5ec9e03b
DPL-Zeta <dpl-zeta@DPL-Zetas-iMac.local>
2014-01-27 13:47:01 -0700

    inti commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '970088543077b980f1a7cf7c1caa36a679f169b7afb6e5c731e2c3c01fcc810da64ea8d0e5b99c80b9ecd63f5a71ea7e4dfddc5bf58875dd43e7e24359f754ba'
